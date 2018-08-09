dccbdaf22c05c7b388e288a7f8bc18b82e9ff4bc
Elise Chavez <elise.a.chavez@gmail.com>
2018-08-09 06:12:24 -0500

    added notes to collatz.py

--- collatz.py
+++ collatz.py
@@ -14,3 +14,13 @@ def collatz(num):
 
 print('Step right up, young lass or gent! Pick a number, any number!!')
 collatz(int(input()))
+
+#####################################
+#NOTE: Did not work the first time, stuck recursively going through 4-3-2-1, 4-3-2-1. 
+#Was a logic error, because I had the escape case (n = 1) as the last case
+#when it should have been the first, so it was never reaching its escape case. 
+#Instead, when the number reached 1 it would go down the n is odd branch and recurse.
+
+#program is 8 lines of code long, when print statements and comments are removed.
+
+#The collatz sequence has been checked for numbers up to 2^60
